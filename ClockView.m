15ee1c36d70872f18cb63c7d0594d0c95797af0d
DB <dbruenner@Davids-MacBook-Pro.local>
2014-09-13 13:29:03 +0200

    3.1.1 Zeichnen in Cocoa Touch

--- ClockView.m
+++ ClockView.m
@@ -33,10 +33,35 @@
 {
     CGContextRef theContext = UIGraphicsGetCurrentContext();
     CGRect theBounds = self.bounds;
+    CGFloat theRadius = CGRectGetWidth(theBounds) / 2.0;
+    
     CGContextSaveGState(theContext);
     CGContextSetRGBFillColor(theContext, 1.0, 1.0, 1.0, 1.0);
     CGContextAddEllipseInRect(theContext, theBounds);
     CGContextFillPath(theContext);
+    CGContextAddEllipseInRect(theContext, theBounds);
+    CGContextClip(theContext);
+    CGContextSetRGBStrokeColor(theContext, 0.25, 0.25, 0.25, 0.25);
+    CGContextSetRGBFillColor(theContext, 0.25, 0.25, 0.25, 0.25);
+    CGContextSetLineWidth(theContext, 8.0);
+    CGContextSetLineCap(theContext, kCGLineCapRound);
+    for (NSInteger i = 0; i < 60; ++i) {
+        CGFloat theAngle = i * M_PI / 30.0;
+        
+        if (i % 5 == 0) {
+            CGFloat theInnerRadius = theRadius * ((i % 15 == 0) ? 0.7 : 0.8);
+            CGPoint theInnerPoint = [self pointWithRadius:theInnerRadius angle:theAngle];
+            CGPoint theOuterPoint = [self pointWithRadius:theRadius angle:theAngle];
+            CGContextMoveToPoint(theContext, theInnerPoint.x, theInnerPoint.y);
+            CGContextAddLineToPoint(theContext, theOuterPoint.x, theOuterPoint.y);
+            CGContextStrokePath(theContext);
+        } else {
+            CGPoint thePoint = [self pointWithRadius:theRadius * 0.95 angle:theAngle];
+            CGContextAddArc(theContext, thePoint.x, thePoint.y, 3.0, 0.0, 2 * M_PI, YES);
+            CGContextFillPath(theContext);
+        }
+    }
+    [self drawClockHands];
     CGContextRestoreGState(theContext);
 }
 
