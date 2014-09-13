9139f072235e32c75d76d5c937bc6de2d44ac5fb
DB <dbruenner@Davids-MacBook-Pro.local>
2014-09-13 10:09:55 +0200

    Initial Commit

---
+++ AlarmClock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlarmClock
+//
+//  Created by David Brünner on 13.09.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
